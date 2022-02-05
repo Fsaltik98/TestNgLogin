b6f7c7dd9ef1c03467acf9cb8a9cba99a6be6d9a
Fsaltik98 <96698494+Fsaltik98@users.noreply.github.com>
2022-02-05 23:11:49 +0000

    Test Valid Invalid input for Login

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,10 @@
 import Utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class LoginTest {
@@ -22,9 +24,82 @@ public class LoginTest {
     }
 
     @Test
-    public void LoginWithValidData(){
+    public void LoginWithValidInput() throws InterruptedException {
 
-       // driver.findElement(By.id("user")).sendKeys();
+        String email = "Employee1";
+        String password = "Employee123";
+        driver.findElement(By.id("user")).sendKeys(email);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit-form")).click();
+        Thread.sleep(5000);
+
+        String name = driver.findElement(By.cssSelector("span[title='Mike Smith']")).getAttribute("title");
+        String expectedName = "Mike Smith";
+
+        Assert.assertEquals(name, expectedName);
     }
+
+    @Test
+    public void LoginWithInValidInput() throws InterruptedException {
+
+        String email = "Ferhat";
+        String password = "Saltik321";
+        driver.findElement(By.id("user")).sendKeys(email);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit-form")).click();
+        Thread.sleep(3000);
+
+        String error = driver.findElement(By.xpath("//p[contains(text(),'Wrong username')]")).getText();
+        String expectedError = "Wrong username or password.";
+
+        Assert.assertEquals(error, expectedError);
+    }
+
+
+    @Ignore
+    @Test
+    public void LoginWithEmptyInputs() throws InterruptedException {
+
+        String email = "";
+        String password = "";
+        driver.findElement(By.id("user")).sendKeys(email);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit-form")).click();
+        Thread.sleep(3000);
+
+        String error = driver.findElement(By.xpath("//p[contains(text(),'Wrong username')]")).getText();
+        String expectedError = "Wrong username or password.";
+
+        Assert.assertEquals(error, expectedError);
+    }
+
+    @Ignore
+    @Test
+    public void LoginWithNull() throws InterruptedException {
+
+        String email = null;
+        String password = null;
+        driver.findElement(By.id("user")).sendKeys(email);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit-form")).click();
+        Thread.sleep(3000);
+
+        String error = driver.findElement(By.xpath("//p[contains(text(),'Wrong username')]")).getText();
+        String expectedError = "Wrong username or password.";
+
+        Assert.assertEquals(error, expectedError);
+    }
+
+    @Test
+    public void CheckIfTextBoxIsEnabled(){
+
+        boolean isEnabled =  driver.findElement(By.id("user")).isEnabled();
+        Assert.assertTrue(isEnabled);
+
+        boolean isEnabledPassword = driver.findElement(By.id("password")).isEnabled();
+        Assert.assertTrue(isEnabledPassword);
+    }
+
+
 
 }
